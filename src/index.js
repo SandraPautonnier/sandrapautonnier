d0e588ea8909f2b2f69841554f00cdb694ccbe8e
SandraPautonnier <sandrapautonnier@gmail.com>
2024-11-18 15:24:13 +0100

    Initialize project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home';
+import Notfound from './pages/Notfound';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/404",
+    element: <Notfound />,
+  },
+  {
+    path: "*",
+    element: <Notfound />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
