c62a8b23b1ce8ff98f634719d51441490f597acf
SandraPautonnier <sandrapautonnier@gmail.com>
2025-02-05 10:50:37 +0100

    updates works

--- src/sections/About/index.jsx
+++ src/sections/About/index.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <section className='about'id='profil'>
         <h2>Qui suis-je?</h2>
         <p>Je m'appelle Sandra Pautonnier, j'ai <AgeCalculator birthDate="1992-07-28" /> ans. <br />
-        Je viens de terminer ma formation d’Intégratrice Web chez OpenClassrooms. Curieuse et créative, je m’intéresse à de nombreux domaines et cherche constamment à comprendre comment les choses fonctionnent. Mon autonomie me permet de relever des défis variés et d’acquérir rapidement de nouvelles compétences. Grâce à cela, j’ai une riche expérience dans différents domaines, ce qui fait de moi une Intégratrice Web polyvalente et innovante.
+        Je viens de terminer ma<strong> formation d’Intégratrice Web chez OpenClassrooms</strong>. Curieuse et créative, je m’intéresse à de nombreux domaines et cherche constamment à comprendre comment les choses fonctionnent. Mon autonomie me permet de relever des défis variés et d’acquérir rapidement de nouvelles compétences. Grâce à cela, j’ai une riche expérience dans différents domaines, ce qui fait de moi une Intégratrice Web polyvalente et innovante.
         </p>
         <div className='tag-btn'>
           <ul className='tags'>
