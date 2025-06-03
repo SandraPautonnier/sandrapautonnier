618996c778270e6653fa4a140b6eaf188e96eb6a
SandraPautonnier <sandrapautonnier@gmail.com>
2025-06-03 14:45:55 +0200

    update meta

--- src/components/Meta/index.jsx
+++ src/components/Meta/index.jsx
@@ -19,33 +19,7 @@ function Meta({ title, description, image, favicon }) {
         document.head.appendChild(metaDescription);
       }
     }
-
-    // Modifier ou créer la meta og:image
-    if (image) {
-      let metaImage = document.querySelector('meta[property="og:image"]');
-      if (metaImage) {
-        metaImage.setAttribute('content', image);
-      } else {
-        metaImage = document.createElement('meta');
-        metaImage.setAttribute('property', 'og:image');
-        metaImage.content = image;
-        document.head.appendChild(metaImage);
-      }
-    }
-
-    // Modifier ou créer le favicon
-    if (favicon) {
-      let linkFavicon = document.querySelector('link[rel="icon"]');
-      if (linkFavicon) {
-        linkFavicon.setAttribute('href', favicon);
-      } else {
-        linkFavicon = document.createElement('link');
-        linkFavicon.rel = 'icon';
-        linkFavicon.href = favicon;
-        document.head.appendChild(linkFavicon);
-      }
-    }
-  }, [title, description, image, favicon]);
+  }, [title, description]);
 
   return null; // Ne rend rien
 }
